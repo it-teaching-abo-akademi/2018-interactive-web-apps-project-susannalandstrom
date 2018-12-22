9b5f9d2808be0dec5bd20111285a98c5e7f203ec
susannalandstrom <landstrom.susanna@gmail.com>
2018-12-22 15:47:00 +0200

    Updates

---
+++ precache-manifest.8dcb45e4ba16a5d7e0ec531c16e96695.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "087accdaf8087abb8fa3",
+    "url": "/2018-interactive-web-apps-project-susannalandstrom/static/js/runtime~main.087accda.js"
+  },
+  {
+    "revision": "ee2f194091fbd75aa19e",
+    "url": "/2018-interactive-web-apps-project-susannalandstrom/static/js/main.ee2f1940.chunk.js"
+  },
+  {
+    "revision": "e05e3eaac6c2eda2b3cc",
+    "url": "/2018-interactive-web-apps-project-susannalandstrom/static/js/1.e05e3eaa.chunk.js"
+  },
+  {
+    "revision": "ee2f194091fbd75aa19e",
+    "url": "/2018-interactive-web-apps-project-susannalandstrom/static/css/main.01571636.chunk.css"
+  },
+  {
+    "revision": "a53487a2a13ea34bde2a27836a513f68",
+    "url": "/2018-interactive-web-apps-project-susannalandstrom/index.html"
+  }
+];
